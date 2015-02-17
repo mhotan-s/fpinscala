31b86f9fbc2eec66be820136853b01e67ff8c752
Michael Hotan <michael.hotan@gmail.com>
2015-02-16 18:01:48 -0800

    Chapter 3 in progress

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -47,11 +47,43 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = sys.error("todo")
+  def tail[A](l: List[A]): List[A] = {
+//    Currently returning Nil for an already Nil list
+//    Matching any
+    l match {
+      case Nil => Nil
+      case Cons(_, t) => t
+    }
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  /**
+   * Replace the head of the list with a new element.
+   *
+   * @param l list to replace.
+   * @param h The new Head element
+   * @tparam A The new list with h at the head
+   * @return List with head replaced
+   */
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("setHead on empty list")
+    case Cons(_, t) => Cons(h, t)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
+  /**
+   * Removes the n elements from a list
+   *
+   * @param l List to remove n items from
+   * @param n Number of head elements to remove
+   * @tparam A The type of the list
+   * @return List with n elements removed.
+   */
+  def drop[A](l: List[A], n: Int): List[A] = {
+    if (n <= 0) l
+    else l match {
+      case Nil => Nil
+      case Cons(_, t) => drop(t, n-1)
+    }
+  }
 
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = sys.error("todo")
 
